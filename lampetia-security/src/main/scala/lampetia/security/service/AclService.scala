63d0c08d4d5892b5522a53cb4ef9b1db6bf43a43
RO86 <radwa.osama@hotmail.com>
2015-06-26 17:10:33 +0200

    Add ACL service

---
+++ lampetia-security/src/main/scala/lampetia/security/service/AclService.scala
@@ -0,0 +1,9 @@
+package lampetia.security.service
+
+/**
+ * @author Radwa Osama
+ * @since 4:44 PM - 6/20/2015
+ */
+class AclService {
+
+}
---
+++ lampetia-security/src/test/scala/lampetia/security/service/AclServiceSpec.scala
@@ -0,0 +1,9 @@
+package lampetia.security.service
+
+/**
+ * @author Radwa Osama
+ * @since 5:10 PM - 6/20/2015
+ */
+class AclServiceSpec {
+
+}
